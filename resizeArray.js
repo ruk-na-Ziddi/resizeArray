3eeab1ffe9be79bf46b1b3399b1457a72462602d
Ankur Kumar Singh <aksinghmt@gmail.com>
2016-07-25 13:09:27 +0530

    resizeArray sets the length of array and adds zeroth element as given value

--- resizeArray.js
+++ resizeArray.js
@@ -1,2 +1,8 @@
 var r = {};
 exports.r = r;
+
+r.resizeArray = function(list, size, element){
+	size && (list.length = size)
+	element && (list[0] = JSON.parse(JSON.stringify(element)))
+	return list
+}
